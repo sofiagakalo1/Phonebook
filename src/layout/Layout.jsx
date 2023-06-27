557ec0d4a720263e0e63ad7228d557b34f84d621
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-06-27 13:44:59 +0200

    Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import NavBar from "../components/NavBar/NavBar";
+import Loader from "../components/Loader/Loader";
+
+import css from "./Layout.module.scss";
 
 const Layout = () => {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <div className={css.container}>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+        <ToastContainer
+          position="top-right"
+          autoClose={1500}
+          theme="colored"
+          hideProgressBar="true"
+        />
+      </div>
+    </>
   );
 };
 
--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Contacts from '../components/Contacts/Contacts';
 
 const ContactsPage = () => {
   return (
     <div>
-      
+      <Contacts/>
     </div>
   )
 }
