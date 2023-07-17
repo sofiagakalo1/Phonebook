e273f21b555fc7c09427fbe4167bd0013a6f43e3
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-07-17 16:49:47 +0300

    adaptive login and register forms

--- src/components/NavBar/AuthMenu/AuthMenu.jsx
+++ src/components/NavBar/AuthMenu/AuthMenu.jsx
@@ -7,12 +7,24 @@ import css from "./AuthMenu.module.scss";
 
 const UserMenu = () => {
   return (
-    <div className={css.div}>
-      <Button navLink={true} text="Register" to={"/register"} hasIcon={true}>
+    <div className={css.container}>
+      <Button
+        navLink={true}
+        text="Register"
+        to={"/register"}
+        hasIcon={true}
+        buttonStyle={{ width: "152px" }}
+      >
         <Icon id="register" h="28" w="28" />
       </Button>
 
-      <Button navLink={true} text="Login" to={"/login"} hasIcon={true}>
+      <Button
+        navLink={true}
+        text="Login"
+        to={"/login"}
+        hasIcon={true}
+        buttonStyle={{ width: "152px" }}
+      >
         <Icon id="login" h="28" w="28" />
       </Button>
     </div>
